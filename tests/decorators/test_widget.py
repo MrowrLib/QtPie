ff60c209d5980b9b75abc25524a712147edb67b5
Mrowr Purr <mrowr.purr@gmail.com>
2025-05-23 15:34:19 -0700

    checkpoint

--- tests/decorators/test_widget.py
+++ tests/decorators/test_widget.py
@@ -10,9 +10,6 @@ from qtpie.decorators.widget import widget, widget_class
 # --- Decorated Widget Classes ---
 
 
-# @widget-decorated widgets
-
-
 @widget
 class WidgetWithoutParentheses(QWidget):
     value: int
@@ -20,11 +17,7 @@ class WidgetWithoutParentheses(QWidget):
 
 @widget()
 class WidgetWithParentheses(QWidget):
-    value_x: int
-    value_y: int
-
-
-# @widget_class-decorated widgets
+    value: int
 
 
 @widget_class
@@ -35,83 +28,44 @@ class PlainWidgetWithoutParentheses(QWidget):
 
 @widget_class()
 class PlainWidgetWithParentheses(QWidget):
-    def __init__(self, value_x: int, value_y: int):
-        self.value_x = value_x
-        self.value_y = value_y
+    def __init__(self, value: int):
+        self.value = value
 
 
-# --- Tests ---
-
-
-class TestWidgetWithoutParentheses:
-    def test_widget_makes_dataclass(self, qtbot: QtBot) -> None:
-        """Test that @widget decorator makes the class a dataclass."""
-        # Arrange & Act
-        widget_instance = WidgetWithoutParentheses(1)
-
-        # Assert
-        assert_that(widget_instance).is_instance_of(WidgetWithoutParentheses)
-        assert_that(widget_instance.value).is_equal_to(1)
-        assert_that(is_dataclass(WidgetWithoutParentheses)).is_true()
-        assert_that(is_dataclass(widget_instance)).is_true()
-
-    def test_widget_sets_object_name(self, qtbot: QtBot) -> None:
-        """Test that @widget decorator sets the object name to the class name."""
-        # Arrange & Act
-        widget_instance = WidgetWithoutParentheses(1)
-
-        # Assert
-        assert_that(widget_instance.objectName()).is_equal_to("WidgetWithoutParentheses")
-
-
-class TestWidgetWithParentheses:
-    def test_widget_makes_dataclass(self, qtbot: QtBot) -> None:
-        """Test that @widget() decorator makes the class a dataclass."""
-        # Arrange & Act
-        widget_instance = WidgetWithParentheses(1, 2)
-
-        # Assert
-        assert_that(widget_instance).is_instance_of(WidgetWithParentheses)
-        assert_that(widget_instance.value_x).is_equal_to(1)
-        assert_that(widget_instance.value_y).is_equal_to(2)
-        assert_that(is_dataclass(WidgetWithParentheses)).is_true()
-        assert_that(is_dataclass(widget_instance)).is_true()
-
-    def test_widget_sets_object_name(self, qtbot: QtBot) -> None:
-        """Test that @widget() decorator sets the object name to the class name."""
-        # Arrange & Act
-        widget_instance = WidgetWithParentheses(1, 2)
-
-        # Assert
-        assert_that(widget_instance.objectName()).is_equal_to("WidgetWithParentheses")
+# --- Shared Tests ---
 
 
 @pytest.mark.parametrize(
     "widget_class,args,expected_name",
     [
         (WidgetWithoutParentheses, (1,), "WidgetWithoutParentheses"),
-        (WidgetWithParentheses, (1, 2), "WidgetWithParentheses"),
+        (WidgetWithParentheses, (2,), "WidgetWithParentheses"),
         (PlainWidgetWithoutParentheses, (42,), "PlainWidgetWithoutParentheses"),
-        (PlainWidgetWithParentheses, (1, 2), "PlainWidgetWithParentheses"),
+        (PlainWidgetWithParentheses, (99,), "PlainWidgetWithParentheses"),
     ],
 )
 class TestSharedWidgetBehavior:
-    def test_sets_object_name(
+    def test_constructor_and_object_name(
         self,
         qtbot: QtBot,
         widget_class: type[QWidget],
-        args: tuple[object, ...],
+        args: tuple[int, ...],
         expected_name: str,
     ) -> None:
+        """Test that all widgets set object name and constructor args correctly."""
         widget_instance = widget_class(*args)
         assert_that(widget_instance.objectName()).is_equal_to(expected_name)
+        assert_that(widget_instance.value).is_equal_to(args[0])
+
+
+# --- Decorator-Specific Tests ---
 
 
 @pytest.mark.parametrize(
     "widget_class,args",
     [
         (WidgetWithoutParentheses, (1,)),
-        (WidgetWithParentheses, (1, 2)),
+        (WidgetWithParentheses, (2,)),
     ],
 )
 class TestDataclassWidgets:
@@ -119,7 +73,7 @@ class TestDataclassWidgets:
         self,
         qtbot: QtBot,
         widget_class: type[QWidget],
-        args: tuple[object, ...],
+        args: tuple[int, ...],
     ) -> None:
         widget_instance = widget_class(*args)
         assert_that(is_dataclass(widget_class)).is_true()
@@ -130,7 +84,7 @@ class TestDataclassWidgets:
     "widget_class,args",
     [
         (PlainWidgetWithoutParentheses, (42,)),
-        (PlainWidgetWithParentheses, (1, 2)),
+        (PlainWidgetWithParentheses, (99,)),
     ],
 )
 class TestPlainWidgets:
@@ -138,17 +92,19 @@ class TestPlainWidgets:
         self,
         qtbot: QtBot,
         widget_class: type[QWidget],
-        args: tuple[object, ...],
+        args: tuple[int, ...],
     ) -> None:
         widget_instance = widget_class(*args)
         assert_that(is_dataclass(widget_class)).is_false()
         assert_that(is_dataclass(widget_instance)).is_false()
 
 
+# --- Validation Tests ---
+
+
 class TestWidgetValidation:
     def test_widget_rejects_non_qwidget(self) -> None:
         """Test that @widget decorator rejects classes that don't inherit from QWidget."""
-        # Arrange & Act & Assert
         with pytest.raises(TypeError, match="@widget can only be applied to QWidget subclasses"):
 
             @widget
